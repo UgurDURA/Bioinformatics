d7598fe83d122d3ce59fd095aa85a49b42b054fd
Uğur DURA <47089334+UgurDURA@users.noreply.github.com>
2022-05-13 23:10:08 +0300

    Initialized

---
+++ Project/dataverse_files/GWAS_Analysis.R
@@ -0,0 +1,8 @@
+
+data(dropsMarkers)
+data(dropsMap)
+data(dropsPheno)
+
+## Add genotypes as row names of dropsMarkers and drop Ind column.
+rownames(dropsMarkers) <- dropsMarkers[["Ind"]]
+dropsMarkers <- dropsMarkers[colnames(dropsMarkers) != "Ind"]
